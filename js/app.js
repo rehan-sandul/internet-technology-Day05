ad6266f22542f9744d8adb9267eca29bf4bee71f
Rehan3807 <rehansandulsatharasinghe@gmail.com>
2026-07-24 08:48:39 +0530

    Refactor app.js: uncomment variable declarations and type checks, remove commented-out code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 /* console.log("hello world"); */
-let name ="saman";
-console.log(typeof name);
+//let name ="saman";
+//console.log(typeof name);
 
-// browser alert
-alert("hello world");
+// -----browser alert-------
+//alert("hello world");
+
+//---------------------
+let age = 20;
+let name = "saman";
+let isStudent = true;
+let Student = [];//array
+let customer = {};//object
+let salary =7400;
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof Student);
+console.log(typeof customer);
+console.log(typeof salary);

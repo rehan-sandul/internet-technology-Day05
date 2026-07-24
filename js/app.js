669fe505489d81dcfb144ca90d3ab96ed0a117ed
Rehan3807 <rehansandulsatharasinghe@gmail.com>
2026-07-24 09:21:07 +0530

    Add conditional statements example in app.js for demonstration

--- js/app.js
+++ js/app.js
@@ -50,4 +50,15 @@ console.log(x === y); */
     console.log("false");
 } */
 
-//
+//---------conditional statements---------
+//if | else if | else
+    
+let x = 10;
+let y = 20;
+if (x > y) {
+    console.log("x is greater than y");
+} else if (x < y) {
+    console.log("x is less than y");
+} else {
+    console.log("x is equal to y");
+}
